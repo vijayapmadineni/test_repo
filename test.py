c067aa39df522dd28b67884d7b877687e79748f6
vijayaprathap <63461673+vijayaprathap@users.noreply.github.com>
2024-05-14 18:12:19 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,18 +1,35 @@
 import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from awsglue.dynamicframe import DynamicFrame
+from pyspark.sql.functions import lit, unix_timestamp
+from pyspark.sql.types import *
+import boto3
+import psycopg2
+from datetime import datetime, timedelta
+import pytz
+import logging
+import re
 import requests
 import zipfile
 import io
 import json
-from pyspark.context import SparkContext
-from pyspark.sql import SparkSession
 
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
 
-import requests
-import json
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+job_name=args['JOB_NAME']
 
 # Fetching the JSON data from the URL
-url = "https://api.fda.gov/download.json"
-response = requests.get(url)
+src_url = "https://api.fda.gov/download.json"
+response = requests.get(src_url)
 data = response.json()
 
 # Extract the NDC URL
@@ -20,13 +37,6 @@ ndc_url = data['results']['drug']['ndc']['partitions'][0]['file']
 
 # Continue processing as needed
 print(ndc_url)
-
-# Initialize Spark Context and Spark Session
-sc = SparkContext()
-spark = SparkSession(sc)
-
-# URL to download the NDC directory ZIP file
-url = "https://download.open.fda.gov/drug/ndc/drug-ndc-0001-of-0001.json.zip"
 
 # Function to download and extract ZIP file
 def download_and_extract_zip(url):
@@ -39,6 +49,7 @@ def download_and_extract_zip(url):
         with zipfile.ZipFile(io.BytesIO(response.content)) as the_zip:
             # Extract all files in the ZIP
             for file_name in the_zip.namelist():
+                print(f'file_name={file_name}')
                 with the_zip.open(file_name) as file:
                     # Read the JSON content
                     json_data = json.load(file)
@@ -49,16 +60,17 @@ def download_and_extract_zip(url):
         print(f"Other error occurred: {err}")
 
 # Download and extract the NDC data
-ndc_data = download_and_extract_zip(url)
+ndc_data = download_and_extract_zip(ndc_url)
 
 # Convert to Spark DataFrame
 if ndc_data:
-    # Assuming the JSON data is a list of dictionaries
+    # # Assuming the JSON data is a list of dictionaries
     spark_df = spark.read.json(sc.parallelize([json.dumps(ndc_data)]))
 
-    # Show schema and data
-    spark_df.printSchema()
-    spark_df.show()
+    # # Show schema and data
+    # spark_df.printSchema()
+    # spark_df.show()
+    print('ndc data was read')
 
 # Stop Spark Context
-sc.stop()
+job.commit()
